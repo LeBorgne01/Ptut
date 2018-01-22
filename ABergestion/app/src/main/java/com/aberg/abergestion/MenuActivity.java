d104554a473f9ca18c9d9e07085ae65c90a14f00
BREVET MAEL <mael.brevet@gmail.com>
2018-01-22 17:20:33 +0100

    Bouton budget/stock

--- ABergestion/app/src/main/java/com/aberg/abergestion/MenuActivity.java
+++ ABergestion/app/src/main/java/com/aberg/abergestion/MenuActivity.java
@@ -1,9 +1,11 @@
 package com.aberg.abergestion;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -23,21 +25,27 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-
-        //name = findViewById(R.id.textView5);
         gestionBudget = findViewById(R.id.button_gestionBudget);
         gestionStock = findViewById(R.id.button_gestionStock);
 
-
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String nom = preferences.getString("NAME","XXX");
-
-        //name.setText(nom);
-
-
-
-
-
-
+        gestionBudget.setOnClickListener(btnGestionBudget);
+        gestionStock.setOnClickListener(btnGestionStock);
+        
     }
+
+    private View.OnClickListener btnGestionBudget = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MenuActivity.this, BudgetActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener btnGestionStock = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MenuActivity.this, StockActivity.class);
+            startActivity(intent);
+        }
+    };
 }
