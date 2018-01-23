3b69a95f0923d637350f4a376f412dac782fe696
BREVET MAEL <mael.brevet@gmail.com>
2018-01-23 10:25:05 +0100

    commentaire

--- ABergestion/app/src/main/java/com/aberg/abergestion/ModifyPasswordActivity.java
+++ ABergestion/app/src/main/java/com/aberg/abergestion/ModifyPasswordActivity.java
@@ -16,6 +16,7 @@ import android.widget.EditText;
 
 public class ModifyPasswordActivity extends AppCompatActivity {
 
+    //On crée les différents composants du XML
     private EditText lastPassword;
     private EditText newPassword;
     private EditText confirmPassword;
@@ -26,11 +27,13 @@ public class ModifyPasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_password);
 
+        //On lie le JAVA et le XML
         lastPassword = findViewById(R.id.passwordNum_lastPassword);
         newPassword = findViewById(R.id.passwordNum_newPassword);
         confirmPassword = findViewById(R.id.passwordNum_confirmPassword);
         valider = findViewById(R.id.button_validerModifyPW);
 
+        //On ajoute un listener pour le bouton
         valider.setOnClickListener(btnValider);
 
     }
@@ -38,26 +41,40 @@ public class ModifyPasswordActivity extends AppCompatActivity {
     private View.OnClickListener btnValider = new View.OnClickListener(){
         @Override
         public void onClick(View v) {
+            //On récupère ce que contiennent les trois champs mot de passe
             String lastPW = lastPassword.getText().toString();
             String newPW = newPassword.getText().toString();
             String confirmPW = confirmPassword.getText().toString();
 
+            //On récupère le mot de passe dans le fichier
             SharedPreferences user = PreferenceManager.getDefaultSharedPreferences(ModifyPasswordActivity.this);
             String password = user.getString("PASSWORD","n/a");
 
+            //On vérifie si les champs sont remplis
             if(isEmpty(lastPW) || isEmpty(newPW) || isEmpty(confirmPW)){
                 alertDialog("Tous les champs doivent être remplis");
             }
             else{
+                //On vérifie que les champs contiennent que des chiffres
                 if(isDigit(lastPW) && isDigit(newPW) && isDigit(confirmPW)){
+
+                    //On vérifie que l'ancien mot de passe est égal au mot de passe du fichier
                     if(password.equals(lastPW)){
+
+                        //On compare les deux nouveaux mots de passe
                         if(comparePassword(newPW,confirmPW)){
+
+                            //On met à jour le mot de passe dans le fichier
                             SharedPreferences.Editor editor = user.edit();
                             editor.putString("PASSWORD", newPW);
                             editor.commit();
+
+                            //On réinitialise les champs
                             lastPassword.setText("");
                             newPassword.setText("");
                             confirmPassword.setText("");
+
+                            //On alerte l'utilisateur
                             alertDialog("Votre mot de passe a été modifié");
                         }
                         else{
