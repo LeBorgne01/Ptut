3b86285a86b47413a7eb61965c57983e3efca1de
BREVET MAEL <mael.brevet@gmail.com>
2018-01-24 09:56:35 +0100

    commentaires groceryliste

--- ABergestion/app/src/main/java/com/aberg/abergestion/GroceryListActivity.java
+++ ABergestion/app/src/main/java/com/aberg/abergestion/GroceryListActivity.java
@@ -22,57 +22,54 @@ import java.util.List;
 
 
 public class GroceryListActivity extends AppCompatActivity {
+
     private Button back;
     private ListView stockListView;
     private List<String> al_groceryList;
 
 
-
-
-
-
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery_list);
 
-        al_groceryList= new ArrayList<String>();
+        al_groceryList = new ArrayList<String>();
+
         back=findViewById(R.id.button_back);
+        stockListView = (ListView) findViewById(R.id.listViewStock);
         back.setOnClickListener(BtnBack);
 
         al_groceryList.add("eau");
         al_groceryList.add("chips");
         al_groceryList.add("ravioli");
 
+        // Définition des colonnes
+        // SimpleCursorAdapter a besoin obligatoirement d'un ID nommé "_id"
+        // Ensuite on met le nombre de colonnes que l'on veut
         String[] colums = new String[]{"_id","col1","col2"};
+
+        // Définition des données du tableau
+        // On affecte au matrixCursor les colonnes que l'on vient de créer
+        // On démarre le MatrixCursor
         MatrixCursor matrixCursor = new MatrixCursor(colums);
-
         startManagingCursor(matrixCursor);
-        matrixCursor.addRow(new Object []{1,"col1:ligne1","col2:ligne1"});
-        matrixCursor.addRow(new Object []{2,"col1:ligne2","col2:ligne2"});
 
+        //On ajoute des objets au MatrixCursor
+        matrixCursor.addRow(new Object[]{1, "col1:ligne1", "col2:ligne1"});
+        matrixCursor.addRow(new Object[]{2, "col1:ligne2", "col2:ligne2"});
+
+        // On prend les données des colonnes 1 et 2 ...
         String[]from = new String []{"col1","col2"};
-        int[] to = new int []{R.id.textView_Col1,R.id.textView_Col2};
+        // ... pour les placer dans les TextView définis dans "row_item.xml"
+        int[] to = new int[] { R.id.textView_Col1, R.id.textView_Col2};
 
+        // On crée l'objet SimpleCursorAdapter
+        // On met le context (this ici), ensuite la définition des lignes de la liste, ensuite on ajoute les lignes, on définit les colonnes, on les lient aux textView, on met le flag à 0
         SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,R.layout.row_item,matrixCursor,from,to,0);
-        stockListView = (ListView) findViewById(R.id.listViewStock);
+
+        // On lie la liste avec l'adapter
         stockListView.setAdapter(adapter);
-        viewGroceryList();
     }
-
-    public void viewGroceryList(){
-
-
-
-        //final ArrayAdapter<String> adapter = new ArrayAdapter<String>(GroceryListActivity.this,android.R.layout.simple_list_item_1, al_groceryList);
-        //ListAdapter adapter = new SimpleAdapter(GroceryListActivity.this, al_groceryList, R.layout.activity_grocery_list, new String[]{"id", "nom"}, new int[] { 1, R.id.name });
-       // stockListView.setAdapter(adapter);
-
-
-
-    }
-
 
     private View.OnClickListener BtnBack = new View.OnClickListener(){
         @Override
