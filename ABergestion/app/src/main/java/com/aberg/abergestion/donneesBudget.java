7efa17fde4f53a57cff0857a74fc496b489c3bce
Xx-louloudu71-xX <louisguillotin.lg@gmail.com>
2018-01-25 09:16:22 +0100

    fe
    f

--- ABergestion/app/src/main/java/com/aberg/abergestion/donneesBudget.java
+++ ABergestion/app/src/main/java/com/aberg/abergestion/donneesBudget.java
@@ -1,18 +1,22 @@
 package com.aberg.abergestion;
 
+import java.io.Serializable;
+
 /**
  * Created by louis on 23/01/2018.
  */
 
-public class donneesBudget {
+public class donneesBudget implements Serializable{
     private String intitule;
     private Date date;
     private double montant;
+    private boolean periodicite;
 
-    public donneesBudget(String inti,Date d, double montant){
+    public donneesBudget(String inti,Date d, double montant,boolean periodicite){
         this.date = d;
         this.intitule= inti;
         this.montant = montant;
+        this.periodicite = periodicite;
     }
 
     public String getIntitule(){
@@ -25,5 +29,13 @@ public class donneesBudget {
 
     public double getMontant(){
         return montant;
+    }
+
+    public boolean isPeriodicite() {
+        return periodicite;
+    }
+
+    public void displayDonneesBudget(){
+        System.out.println("Intitule : " + intitule+"\nDate : "+date.displayDate()+"\nMontant :"+montant+"\nPeriodicite :"+periodicite);
     }
 }
